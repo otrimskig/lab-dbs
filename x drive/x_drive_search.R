b3718d163d1527276df4463ab02533fe99a1d008
Garrett Otrimski <u1413890@hci.utah.edu>
2023-01-23 13:24:34 -0700

    x

--- x drive/x_drive_search.R
+++ x drive/x_drive_search.R
@@ -5,15 +5,15 @@ library(janitor)
 readRDS("x drive/x_drive_index.rds")->files
 
 
-# #folder
-# search_term<-paste0("clara", "\\\\")
+#folder
+search_term<-paste0("clara", "\\\\")
 # 
 # #anywhere in path
 # search_term<-"clara"
 
 
 #in filename
-search_term<-paste0(".*\\/*", "nanodrop", "*")
+# search_term<-paste0(".*\\/*", "nanodrop", "*")
 
 
 
@@ -24,22 +24,23 @@ files%>%
   filter(grepl(search_term, path, ignore.case = TRUE))%>%
   view()
 
-files%>%
-  filter(grepl(search_term, path, ignore.case = TRUE))->test
+
+# files%>%
+#   filter(grepl(search_term, path, ignore.case = TRUE))->test
 
 
 
 
 
-
-test%>%
-  slice(1, 7, 8, 12)%>%
-  select(path)%>%
-  
-
-  filter(str_detect(path, regex("nanodrop(?!\\\\)", ignore_case = TRUE)))%>%
-  
-view()
+# 
+# test%>%
+#   slice(1, 7, 8, 12)%>%
+#   select(path)%>%
+#   
+# 
+#   filter(str_detect(path, regex("nanodrop(?!\\\\)", ignore_case = TRUE)))%>%
+#   
+# view()
 
 
 
